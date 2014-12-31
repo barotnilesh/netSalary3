66d12e5ad32ef711fe29dd055759a6b6ddc4391b
Nilesh Barot <nilesh.barot@itv.com>
2014-12-31 15:39:46 +0000

    Initial Commit

---
+++ netSalary3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netSalary3
+//
+//  Created by nilebaro on 31/12/2014.
+//  Copyright (c) 2014 itvolunteersuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
